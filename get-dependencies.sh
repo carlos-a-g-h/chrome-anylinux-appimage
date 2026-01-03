8dfd2b17ba79827cf1f018bacf249aa65a909fd9
carlos-a-g-h <carloselputohabanero@proton.me>
2026-01-03 17:29:57 -0500

    Add files via upload

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,18 +1,44 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 ARCH=$(uname -m)
 
-echo "Installing package dependencies..."
-echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+URL_RPM="$(sed -n 1p latest_version.txt)"
+URL_GPG="$(sed -n 2p latest_version.txt)"
+URL_SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+echo "Downloading scripts..."
+echo "----------------------"
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+dnf update -y
 
-# If the application needs to be manually built that has to be done down here
+dnf in -y wget xorg-x11-server-Xvfb patchelf binutils zstd fastfetch zsync strace
+
+FILE_RPM=$(basename "${URL_RPM:7}")
+if ! [ -f $FILE_RPM ]
+then
+	wget "$URL_RPM" -O "$FILE_RPM"
+fi
+
+FILE_GPG=$(basename "${URL_GPG:7}")
+if ! [ -f $FILE_GPG ]
+then
+	wget "$URL_GPG" -O "$FILE_GPG"
+fi
+
+FILE_SHARUN=$(basename "${URL_SHARUN:7}")
+if ! [ -f $FILE_SHARUN ]
+then
+	wget "$URL_SHARUN" -O "$FILE_SHARUN"
+	chmod +x "$FILE_SHARUN"
+fi
+
+echo "Installing package..."
+echo "---------------------"
+
+rpm --import "$FILE_GPG"
+
+rpm -K "$FILE_RPM"
+
+dnf in -y $(realpath -e "$FILE_RPM")
--- make-appimage.sh
+++ make-appimage.sh
@@ -2,19 +2,69 @@
 
 set -eu
 
-ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+ARCH="$(uname -m)"
+VERSION="v$(sed -n 1p version.txt)"
+
+UBID="$1"
+UBID_SHORT="${UBID:0:8}"
+
+NAME="GoogleChrome"
+APPIMAGE_STEM="$NAME"_"$VERSION"_"$UBID_SHORT"_anylinux_"$ARCH"
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
-export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+#export ADD_HOOKS="self-updater.bg.hook"
+#export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON="/opt/google/chrome/product_logo_256.png"
+export OUTNAME="$APPIMAGE_STEM".AppImage
+export DESKTOP="/usr/share/applications/google-chrome.desktop"
+
+export DEPLOY_LOCALE=1
+export DEPLOY_OPENGL=0
+export DEPLOY_VULKAN=0
+export DEPLOY_GEGL=0
+export DEPLOY_PULSE=1
+export DEPLOY_PIPEWIRE=0
+export DEPLOY_GTK=0
+export DEPLOY_QT=0
+export DEPLOY_SDL=0
+export DEPLOY_GLYCIN=0
+
+mkdir -p AppDir/bin
+
+cp -va /opt/google/chrome/* AppDir/bin/
+
+cat "$DESKTOP" | sed -e 's|google-chrome-stable|google-chrome-stable --no-sandbox|' -e 's|Name=Google Chrome|Name=Google Chrome (No Sandbox)|' > AppDir/google-chrome-no-sandbox.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+
+./quick-sharun.sh ./AppDir/bin/*
 
 # Additional changes can be done in between here
 
+# Copy the config
+#if [ -d _config ]
+#then
+#	mkdir -p AppDir/_config
+#	cp -va _config/* AppDir/_config/
+#fi
+
+# Copy details
+mkdir -v AppDir/_details
+echo "$UBID" > AppDir/_details/commit.txt
+echo "$(date)" > AppDir/_details/date.txt
+rpm -qa > AppDir/_details/packages.txt
+cp -v latest_version.txt > AppDir/_details/
+fastfetch|sed -e 's/Local IP.*//' -e 's/Locale.*//' -e 's/Battery.*//' -e 's/Disk.*//' -e 's/Swap.*//' > AppDir/_details/system.txt
+
+# Copy Internal scripts
+# mkdir -vp AppDir/bin
+
+cp -v is_details AppDir/bin/details
+cp -v is_setup.1.sh AppDir/bin/setup
+cat is_setup.2.sh >> AppDir/bin/setup
+
+chmod +x AppDir/bin/details
+chmod +x AppDir/bin/setup
+
 # Turn AppDir into AppImage
-quick-sharun --make-appimage
+./quick-sharun.sh --make-appimage
